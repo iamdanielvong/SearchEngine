0f8789a4239f065c84f2df6ce770163f26e51f9d
Daniel <iamdanielvong@gmail.com>
2020-02-16 01:04:15 -0800

    Turned Bookkeeping.json into a dict

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
+import json
 
 if __name__ == "__main__":
-    print("hello worlds")
-    directory = "/Users/daniel/Downloads/WEBPAGES_CLEAN"
+
+    directory = "/Users/daniel/Downloads/WEBPAGES_CLEAN/"
+    docId_url_dict = {}
+
     for folder in os.listdir(directory):
-        #print(folder)
-        if folder.endswith(".json") or folder.endswith(".tsv"):
-            print("bookkeeping files")
+        if folder.endswith(".json"):
+            with open(directory + folder) as f:
+                docId_url_dict = json.load(f)
+            break
+
+    for folder in os.listdir(directory):
+        if folder.endswith(".tsv"):
             continue
-        for file in os.listdir("/Users/daniel/Downloads/WEBPAGES_CLEAN/" + folder):
-            print(os.path.join(folder, file))
+        if folder.endswith(".json"):
+            continue
+
+        for file in os.listdir(directory + folder):
+            docID = os.path.join(folder, file)
+            print("DocID: " + docID + " URL: " + docId_url_dict[docID])
+
